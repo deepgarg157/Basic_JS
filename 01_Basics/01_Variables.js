943ba7cc46b8c71534583c0c80c3eacd33b1d0d3
deepgarg157 <139165184+deepgarg157@users.noreply.github.com>
2023-08-02 08:08:18 +0000

    variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountID=12345;
+let accountEmail="Hello@gmail.com";
+var accountPassword="12345";
+accountcity="Noida";
+let accountState;
+
+// where var is not use in the java script
+// beacause of issue in block scope & functional scope
+
+console.table([accountID, accountEmail, accountPassword, accountcity, accountState]);
