11507fa3d521e85e8120993b08146b502adf755b
deepgarg157 <139165184+deepgarg157@users.noreply.github.com>
2023-08-02 14:32:04 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,6 @@
+console.log(typeof "Deepanshu Garg");
+console.log(typeof 123);
+console.log(null)
+console.log(true)
+let a;
+console.log(a);
---
+++ 01_Basics/03_Datatype_conversion.js
@@ -0,0 +1,20 @@
+// let score="33aa";
+// let score= null
+// let score=undefined
+let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let number=0;
+// let booleanNumber=Boolean(number)
+// console.log(booleanNumber)
+
+let number=33;
+let stringNumber=String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
