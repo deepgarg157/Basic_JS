ca51ec44870d4ca41116b588a5acf9d0288ce6e1
deepgarg157 <139165184+deepgarg157@users.noreply.github.com>
2023-08-03 12:00:58 +0000

    update

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,29 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// =====================Stack & Heap concept===================
+
+// stack
+let name1="Deepgarg";
+let anotherName=name1;
+anotherName="deepgarg157";
+console.log(name1);
+console.log(anotherName);
+
+// heap
+
+let Userone={
+    email:"deep@gmail.com",
+    upiID:"deeg@ybl"
+}
+let UserTwo=Userone;
+UserTwo.email="hello@gmail.com"
+
+console.log(Userone.email);
+console.log(UserTwo.email);
