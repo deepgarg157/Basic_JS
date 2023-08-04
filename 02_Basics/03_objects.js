e45249518c6773da740f8606579925698808d497
deepgarg157 <139165184+deepgarg157@users.noreply.github.com>
2023-08-04 13:29:50 +0000

    object

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,34 @@
+// object
+
+const mySym = Symbol("key1")
+
+const JSuser={
+    name:"Deepanshu",
+    "full name":"Deepanshu garg",
+    age:25,
+    email:"Deepgarg@gmail.com",
+    location:"Noida",
+    isLoggedIN:false,
+    lastDaysLogged:["Monday","Tuesday"],
+    [mySym]:"myKey1"
+}
+
+// console.log(JSuser.email);
+// console.log(JSuser["name"]);
+// console.log(JSuser["full name"]);
+
+JSuser.email="deepgarg777@gmail.com"
+// console.log(JSuser.email);
+
+
+
+JSuser.greeting=function(){
+    console.log("Hello user");
+}
+
+JSuser.greetingNew=function(){
+    console.log(`hello user ${this["full name"]}`);
+}
+
+console.log(JSuser.greeting());
+console.log(JSuser.greetingNew());
