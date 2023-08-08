ff0c00fff0c33ec6399c5230eeb8deb26bb6c307
deepgarg157 <139165184+deepgarg157@users.noreply.github.com>
2023-08-08 15:39:56 +0000

    update

---
+++ 04_control_flow_Conditional_Statement/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Switch case syntax:-
+
+let month="march"
+switch (month) {
+    case "jan":
+        console.log("This is a jan month");
+        break;
+
+        case "feb":
+        console.log("This is a feb month");
+        break;
+
+        case "march":
+        console.log("This is a march month");
+        break;
+
+        case "april":
+        console.log("This is a april month");
+        break;
+
+    default:
+        console.log("Invalid month");
+        break;
+}
