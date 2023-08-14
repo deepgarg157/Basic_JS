80f925caf3cbbbc93705b7392b5152d57b43fdab
deepgarg157 <139165184+deepgarg157@users.noreply.github.com>
2023-08-14 13:16:07 +0000

    update

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -14,7 +14,7 @@ for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
 
-// Maps
+// Maps, filters, reduce in Java Script...
 
 const map = new Map()
 map.set('IN', "India")
